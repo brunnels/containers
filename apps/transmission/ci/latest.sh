501de491911a10981dee8b2a21af93435a32ac6d
brunnels <kraven@kraven.org>
2025-02-19 20:15:12 +0000

    add user-agent for replology api

--- apps/transmission/ci/latest.sh
+++ apps/transmission/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=transmission&inrepo=alpine_3_21" | jq -r '.transmission | .[] | select((.repo == "alpine_3_21" and .binname == "transmission-daemon")) | .origversion')
+version=$(curl -A "brunnels/containers" -sX GET "https://repology.org/api/v1/projects/?search=transmission&inrepo=alpine_3_21" | jq -r '.transmission | .[] | select((.repo == "alpine_3_21" and .binname == "transmission-daemon")) | .origversion')
 version="${version%%_*}"
 version="${version%%-*}"
 printf "%s" "${version}"
